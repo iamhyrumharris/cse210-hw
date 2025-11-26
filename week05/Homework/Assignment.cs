b9cc55a87cf6b944234055d8131d32b5bc46e92c
iamhyrumharris <82340621+iamhyrumharris@users.noreply.github.com>
2025-11-26 06:05:19 -0700

    Inheritance

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,19 @@
+using System.Runtime.CompilerServices;
+
+public class Assignment
+{
+    protected string _studentName;
+    private string _topic;
+
+    public Assignment(string name, string topic)
+    {
+        _studentName = name;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} and {_topic}";
+    }
+
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Hyrum", "Math");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("eliza", "english", "adjectives", "5");
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine(mathAssignment.GetSummary());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Lee", "revolution", "How It Happened");
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
